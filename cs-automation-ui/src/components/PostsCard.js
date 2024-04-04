b1b9c31436852989ab8bcfa236804a8e73241575
khifayatabolurin <abolurinkhifayat1@gmail.com>
2024-04-04 17:50:15 -0500

    added posts cards

---
+++ cs-automation-ui/src/components/PostsCard.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const PostsCard = () =>{  
+    return (
+        <div style={{backgroundColor:"#c8c9c7", border:"solid #D29F13 "}}class="card shadow p-3 ml-1">
+        <div class="card-body">
+          <h3>Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+        </div>
+      </div>
+  );
+}
+
+export default PostsCard;
--- cs-automation-ui/src/pages/Home.js
+++ cs-automation-ui/src/pages/Home.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import AppNavbar from "../components/AppNavbar";
+import PostsCard from "../components/PostsCard";
 
 const Home = () => {
+  const [posts, setPosts] = useState([1,2,3,4,5,6,7,8,9,10])
+
   return(
       <>
+      <div style={{ width:"100%", height:"100%"}}>
       <AppNavbar/>
+      {posts.map((post)=>
+      <PostsCard/>)}
+      </div>
       </>
   )
 }
