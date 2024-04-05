ad11290b4271e23d8f323373faf0c89e9a4a9ec3
khifayatabolurin <abolurinkhifayat1@gmail.com>
2024-04-04 22:47:08 -0500

    new change

--- cs-automation-ui/src/components/PostsCard.js
+++ cs-automation-ui/src/components/PostsCard.js
@@ -8,6 +8,7 @@ const PostsCard = () =>{
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
         </div>
       </div>
+        //change
   );
 }
 
