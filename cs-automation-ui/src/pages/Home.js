2d8f3fea930cd229ac01e555346f7ebfd29e190e
NYLTI <al.firebird@mailfence.com>
2024-04-05 17:51:25 -0400

    formatting

--- cs-automation-ui/src/pages/Home.js
+++ cs-automation-ui/src/pages/Home.js
@@ -3,26 +3,26 @@ import AppNavbar from "../components/AppNavbar";
 import PostsCard from "../components/PostsCard";
 import httpmain from "../services/httpclientmain/httpmain"
 
-function Home(){
+function Home() {
 
-  const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([])
 
-  const getPosts = async()=>{
-      const response = await httpmain.get('/approved')
-      setPosts(response.data)
-  }
-  useEffect(()=>{
-      getPosts()
-  }, [])
-  return(
-      <>
-      <div style={{ width:"100%", height:"100%"}}>
-      <AppNavbar/>
-      {posts?.map((post)=>
-      <PostsCard post = {post}/>)}
-      </div>
-      </>
-  )
+    const getPosts = async () => {
+        const response = await httpmain.get('/approved')
+        setPosts(response.data)
+    }
+    useEffect(() => {
+        getPosts()
+    }, [])
+    return (
+        <>
+            <div style={{ width: "100%", height: "100%" }}>
+                <AppNavbar />
+                {posts?.map((post) =>
+                    <PostsCard post={post} />)}
+            </div>
+        </>
+    )
 }
 
 export default Home;
