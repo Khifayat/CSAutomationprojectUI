19d738af559fb63950ab3b5113482555130d9b09
khifayatabolurin <abolurinkhifayat1@gmail.com>
2024-04-06 19:40:48 -0500

    added env file

--- cs-automation-ui/src/services/httpclientmain/httpmain.js
+++ cs-automation-ui/src/services/httpclientmain/httpmain.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 export default axios.create({
-    baseURL: 'http://localhost:8080/v1'
+    baseURL: `${backendUrl}/v1`
 });
