9ccc0bf9ea0e99da5b7769250a6b356f5084ab13
khifayatabolurin <abolurinkhifayat1@gmail.com>
2024-04-08 08:30:33 -0500

    adding routs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-// import Home from "./pages/Home";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
+
+import Home from "./pages/Home";
 // import RegistrationPage from "./pages/RegistrationPage";
 // import Home from './pages/Home'
 // import LoginPage from './pages/LoginPage'
@@ -6,12 +12,19 @@ import UserDashboardPage from './pages/UserDashboardPage'
 
 function App() {
   return (
-    <div >
-        {/* <RegistrationPage/> */}
-        {/* <Home/> */}
-        {/* <LoginPage/> */}
-        <UserDashboardPage/>
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              {/*<Route path="/about" element={<About />} />*/}
+              {/*<Route path="/contact" element={<Contact />} />*/}
+          </Routes>
+      </Router>
+    // <div >
+    //     {/* <RegistrationPage/> */}
+    //     {/* <Home/> */}
+    //     {/* <LoginPage/> */}
+    //     <UserDashboardPage/>
+    // </div>
   );
 }
 
