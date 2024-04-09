c5fdd30397fb3904fbbfef3b0a3471f79b175f17
NYLTI <al.firebird@mailfence.com>
2024-04-09 10:42:19 -0400

    minor dashboard ui change

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Dashboard = ({post}) =>{  
     return (
       
-        <div style={{backgroundColor:"#c8c9c7", border:"solid #D29F13", minHeight:"600px", height:"600px"}} class="card shadow p-3 ml-1">
+        <div style={{backgroundColor:"#c8c9c7", border:"solid #D29F13", minHeight:"500px"}} class="card shadow p-3 ml-1">
         <div class="card-body">
           <h3>{post.title}</h3>
           <p>{post.description}</p>
