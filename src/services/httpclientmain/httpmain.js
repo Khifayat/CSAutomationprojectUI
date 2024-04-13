5da77c2c19abcf20850efa1ab41546a5e90ad059
NYLTI <al.firebird@mailfence.com>
2024-04-12 23:24:48 -0400

    reload

--- src/services/httpclientmain/httpmain.js
+++ src/services/httpclientmain/httpmain.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const backendUrl = process.env.REACT_APP_API_URL;
 
 export default axios.create({
-    baseURL: `http://localhost:8080/v1`
-    //  baseURL: `${backendUrl}/v1`
+    // baseURL: `http://localhost:8080/v1`
+     baseURL: `${backendUrl}/v1`
 });
